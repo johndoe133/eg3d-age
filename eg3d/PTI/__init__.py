e332ade52e851cb2c4b037d6bdbdee41797b6cf3
MortenBjerre <morten.bjerre1@gmail.com>
2022-09-03 22:52:02 +0200

    initial pti commit

---
+++ eg3d/PTI/__init__.py
@@ -0,0 +1,10 @@
+import os
+from training import volumetric_rendering
+# from PTI import configs, criteria, dnnlib, editings
+# from PTI import evaluation, models, pretrained_models, scripts, torch_utils
+# from PTI.training.projectors import *
+# from PTI.models.e4e.psp import *
+# from PTI.training.coaches.base_coach import BaseCoach
+# from PTI.training.coaches.multi_id_coach import MultiIDCoach
+# from training.coaches.single_id_coach import SingleIDCoach
+
