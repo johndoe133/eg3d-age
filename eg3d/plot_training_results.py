a4ca84dc294731fa54b4ca0e3270552df65b72a8
MortenBjerre <morten.bjerre1@gmail.com>
2022-09-04 12:29:03 +0200

    id loss added to plot

--- eg3d/plot_training_results.py
+++ eg3d/plot_training_results.py
@@ -29,8 +29,8 @@ def generate_images(
     json_filename = 'stats.jsonl'
     path = os.path.join(root, training_run, json_filename)
     stats = pd.read_json(path, lines=True)
-    loss_scores_fake, loss_G, loss_D, r1_penalty, loss_age, hours = [], [], [], [], [], []
-    loss_G_std, loss_D_std, loss_age_std = [],[],[]
+    loss_scores_fake, loss_G, loss_D, r1_penalty, loss_age, loss_id, hours = [], [], [], [], [], [], []
+    loss_G_std, loss_D_std, loss_age_std, loss_id_std = [],[],[],[]
     for row in stats.iterrows():
         data = row[1]
         loss_scores_fake.append(data['Loss/scores/fake']['mean'])
@@ -41,6 +41,8 @@ def generate_images(
         r1_penalty.append(data['Loss/r1_penalty']['mean'])
         loss_age.append(data['Loss/scores/age']['mean'])
         loss_age_std.append(data['Loss/scores/age']['std'])
+        loss_id.append(data['Loss/scores/age']['mean'])
+        loss_id_std.append(data['Loss/scores/age']['std'])
         hours.append(data['Timing/total_hours']['mean'])
     
     fid = []
@@ -52,46 +54,58 @@ def generate_images(
     
 
     #convert to numpy
-    loss_scores_fake, loss_G, loss_D, r1_penalty, loss_age, hours = np.array(loss_scores_fake), np.array(loss_G), np.array(loss_D), np.array(r1_penalty), np.array(loss_age), np.array(hours)
-    loss_G_std, loss_D_std, loss_age_std = np.array(loss_G_std), np.array(loss_D_std), np.array(loss_age_std)
+    loss_scores_fake, loss_G, loss_D, r1_penalty, loss_age, loss_id, hours = np.array(loss_scores_fake), np.array(loss_G), np.array(loss_D), np.array(r1_penalty), np.array(loss_age), np.array(loss_id), np.array(hours)
+    loss_G_std, loss_D_std, loss_age_std, loss_id_std = np.array(loss_G_std), np.array(loss_D_std), np.array(loss_age_std), np.array(loss_id_std)
 
-    fig, axs = plt.subplots(2, 2, figsize=figsize, dpi=dpi)
+    fig = plt.figure(figsize=figsize, dpi=dpi)
+    fig.tight_layout()
     x = range(len(hours))
     
     #GD_ylim = max(list(loss_G + loss_G_std) + list(loss_D + loss_D_std)) * 1.10
     ## GENERATOR LOSS ##
-    axs[0,0].plot(x, loss_G, label="Loss")
-    axs[0,0].set_ylabel("G loss")
-    axs[0,0].fill_between(x, loss_G - loss_G_std, loss_G + loss_G_std, alpha=0.4, label="std")
-    axs[0,0].set_ylim(0,max(loss_G + loss_G_std)*1.10)
-    axs[0,0].legend()    
+    axs1 = fig.add_subplot(3,2,1)
+    axs1.set_ylabel("G loss")
+    axs1.fill_between(x, loss_G - loss_G_std, loss_G + loss_G_std, alpha=0.4, label="std")
+    axs1.set_ylim(0,max(loss_G + loss_G_std)*1.10)
+    axs1.plot(x, loss_G, label="Loss")
+    axs1.legend()    
 
     ## DISCRIMINATOR LOSS ##
-    axs[0,1].plot(x, loss_D, label="Loss")
-    axs[0,1].set_ylabel("D loss")
-    axs[0,1].set_ylim(0,max(loss_D + loss_D_std)*1.10)
-    axs[0,1].fill_between(x, loss_D - loss_D_std, loss_D + loss_D_std, alpha=0.4, label="std")
-    axs[0,1].legend()
-
-    ## AGE LOSS ##
-    axs[1,0].plot(x, loss_age, label="Loss")
-    age_loss_ylim = max(loss_age + loss_age_std)
-    axs[1,0].set_ylabel("Age loss")
-    axs[1,0].set_ylim(0, age_loss_ylim*1.05)
-    axs[1,0].fill_between(x, loss_age - loss_age_std, loss_age + loss_age_std, alpha=0.4, label="std")
-    axs[1,0].legend()
+    axs2 = fig.add_subplot(3,2,3)
+    axs2.plot(x, loss_D, label="Loss")
+    axs2.set_ylabel("D loss")
+    axs2.set_ylim(0,max(loss_D + loss_D_std)*1.10)
+    axs2.fill_between(x, loss_D - loss_D_std, loss_D + loss_D_std, alpha=0.4, label="std")
+    axs2.legend()
 
     ## FID score
-    axs[1,1].set_title("FID Score")
-    axs[1,1].bar(range(len(fid)), fid, zorder=20)
-    axs[1,1].set_xticks([])
-    axs[1,1].grid(axis='y')
-    
+    axs3 = fig.add_subplot(3,2,5)
+    axs3.set_ylabel("FID Score")
+    axs3.bar(range(len(fid)), fid, zorder=20)
+    axs3.set_xticks([])
+    axs3.grid(axis='y')
+
+    ## AGE LOSS ##
+    axs4 = fig.add_subplot(2,2,2)
+    axs4.plot(x, loss_age, label="Loss")
+    age_loss_ylim = max(loss_age + loss_age_std)
+    axs4.set_ylabel("Age loss")
+    axs4.set_ylim(0, age_loss_ylim*1.05)
+    axs4.fill_between(x, loss_age - loss_age_std, loss_age + loss_age_std, alpha=0.4, label="std")
+    axs4.legend()
+
+    ## ID LOSS ##
+    axs5 = fig.add_subplot(2,2,4)
+    axs5.plot(x, loss_id, label="Loss")
+    id_loss_ylim = max(loss_id + loss_id_std)
+    axs5.set_ylabel("ID loss")
+    axs5.set_ylim(0, id_loss_ylim*1.05)
+    axs5.fill_between(x, loss_id - loss_id_std, loss_id + loss_id_std, alpha=0.4, label="std")
+    axs5.legend()
     
     # save figure
     plot_name = name
     plot_path = os.path.join(root, outdir, plot_name)
-    plt.tight_layout()
     fig.savefig(plot_path)
 
     print("Ending program...")
