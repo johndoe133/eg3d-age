2732625dba5586f2177de63186e521467728b94c
MortenBjerre <morten.bjerre1@gmail.com>
2022-10-10 15:09:40 +0200

    eval shell

--- eg3d/Experiments/evaluate.sh
+++ eg3d/Experiments/evaluate.sh
@@ -7,6 +7,6 @@ node_ip="$(ifconfig | grep "inet" | awk 'NR==1{print $2}')"
 export CUDA_VISIBLE_DEVICES=0,1
 
 python -m debugpy \
-    --listen $node_ip:1500 evaluation.py \
-    --network_folder=./training-runs/00122-ffhq-FFHQ_512-gpus2-batch8-gamma5 \
-    --run_eval False
+    --listen $node_ip:1500 Evaluation/run_evaluation.py \
+    --network_folder=./training-runs/00133/00000-ffhq-FFHQ_512_6_balanced-gpus2-batch8-gamma5\
+    --age_model_name v2 --no-img 0 --run_generate_data True
