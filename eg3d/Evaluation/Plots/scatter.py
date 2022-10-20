bdc20410aecba8b574b81441b7657fa533fc2c90
MortenBjerre <morten.bjerre1@gmail.com>
2022-10-20 15:02:08 +0200

    categories

--- eg3d/Evaluation/Plots/scatter.py
+++ eg3d/Evaluation/Plots/scatter.py
@@ -28,7 +28,7 @@ def scatter_plot(network_folder, path):
     else:
         age_min = 0
         age_max = 100
-
+    age_loss_fn = training_option['age_loss_fn']
     fig_name = "scatter"
     save_path = os.path.join("Evaluation", "Runs", path)
     df = pd.read_csv(os.path.join(save_path, "age_scatter.csv"))
@@ -38,46 +38,46 @@ def scatter_plot(network_folder, path):
     mag = df.mag.to_numpy()
     mag_min, mag_max = mag.min(), mag.max()
 
-    if len(categories) > 1:
-        ranges = len(categories) - 1
-        confusion = np.zeros((ranges, ranges))
-        for i, age in enumerate(age_hat):
-            predicted_range = get_age_category(np.array([age]), categories, normalize_category=True)
-            s = age_true[i]
-            s = re.sub(r'\[[0-9\.\s]+\]', add_comma, s)
-            s = re.sub(r'([0-9\.]+)', add_comma, s)
-            true_range = eval(s)[0]
-            x = np.array(predicted_range).nonzero()[0][0]
-            y = np.array(true_range).nonzero()[0][0]
-            confusion[x,y] = confusion[x,y] + 1
-        figsize = compute_figsize(350, 250)
-        plt.figure(figsize=figsize, dpi=300)
-        labels = []
-        for i in range(ranges):
-            labels.append(f"{categories[i]}-{categories[i+1] - 1}")
-        df_cm = pd.DataFrame(confusion, index = labels, columns = labels)
+    # if age_loss_fn == "CAT":
+    # ranges = 101 #todo
+    # confusion = np.zeros((ranges, ranges))
+    # for i, age in enumerate(age_hat):
+    #     predicted_range = get_age_category(np.array([age]), categories, normalize_category=True)
+    #     s = age_true[i]
+    #     s = re.sub(r'\[[0-9\.\s]+\]', add_comma, s)
+    #     s = re.sub(r'([0-9\.]+)', add_comma, s)
+    #     true_range = eval(s)[0]
+    #     x = np.array(predicted_range).nonzero()[0][0]
+    #     y = np.array(true_range).nonzero()[0][0]
+    #     confusion[x,y] = confusion[x,y] + 1
+    # figsize = compute_figsize(350, 250)
+    # plt.figure(figsize=figsize, dpi=300)
+    # labels = []
+    # for i in range(ranges):
+    #     labels.append(f"{categories[i]}-{categories[i+1] - 1}")
+    # df_cm = pd.DataFrame(confusion, index = labels, columns = labels)
 
-        sn.set(font_scale=1.4) # for label size
-        sn.heatmap(df_cm, annot=True, annot_kws={"size": 16}) # font size
+    # sn.set(font_scale=1.4) # for label size
+    # sn.heatmap(df_cm, annot=True, annot_kws={"size": 16}) # font size
 
-    else:
-        viridis = cm.get_cmap('winter', 12)
-        figsize = compute_figsize(350, 250)
-        age_hat = denormalize(age_hat, rmin=age_min, rmax=age_max)
-        age_true = denormalize(age_true, rmin=age_min, rmax=age_max)
-        idx = normalize(mag, rmin=mag_min, rmax=mag_max, tmin=0)
-        colors = viridis(idx)
-        corr, _ = pearsonr(age_hat, age_true)
-        corr = round(corr, 4)
-        plt.figure(figsize=figsize, dpi=300)
-        plt.xticks(np.linspace(0, (age_max//10)*10, (age_max//10)+1))
-        plt.yticks(np.linspace(0, (age_max//10)*10, (age_max//10)+1))
-        plt.scatter(age_true, age_hat, s=5, c=mag, cmap="winter")
-        plt.plot([age_min, age_max], [age_min, age_max], '--', label="Perfect prediction", color='black')
-        plt.xlabel("True age")
-        plt.ylabel("Predicted age")
-        plt.colorbar(label = "MagFace magnitude")
-        plt.legend()
+    # else:
+    viridis = cm.get_cmap('winter', 12)
+    figsize = compute_figsize(350, 250)
+    age_hat = denormalize(age_hat, rmin=age_min, rmax=age_max)
+    age_true = denormalize(age_true, rmin=age_min, rmax=age_max)
+    idx = normalize(mag, rmin=mag_min, rmax=mag_max, tmin=0)
+    colors = viridis(idx)
+    corr, _ = pearsonr(age_hat, age_true)
+    corr = round(corr, 4)
+    plt.figure(figsize=figsize, dpi=300)
+    plt.xticks(np.linspace(0, (age_max//10)*10, (age_max//10)+1))
+    plt.yticks(np.linspace(0, (age_max//10)*10, (age_max//10)+1))
+    plt.scatter(age_true, age_hat, s=5, c=mag, cmap="winter")
+    plt.plot([age_min, age_max], [age_min, age_max], '--', label="Perfect prediction", color='black')
+    plt.xlabel("True age")
+    plt.ylabel("Predicted age")
+    plt.colorbar(label = "MagFace magnitude")
+    plt.legend()
     plt.savefig(save_path + f"/{fig_name+str(corr)}" + ".png",bbox_inches='tight')
     plt.savefig(save_path + f"/{fig_name+str(corr)}" + ".pgf",bbox_inches='tight')
     print(f"Figure {fig_name+str(corr)} save at {save_path}")
