449ebdbd5a59cdb0c9f07fcefc962dfeada29db6
MortenBjerre <morten.bjerre1@gmail.com>
2022-11-16 14:05:36 +0100

    where to download

--- eg3d/networks/dex/models.py
+++ eg3d/networks/dex/models.py
@@ -45,7 +45,7 @@ class VGG(nn.Module):
 
     def forward(self, x):
         in_size = x.shape[0]
-        x = self.conv(x)
+        x = self.conv(x).contiguous()
         x = x.view(in_size, -1)
         x = self.fc1(x)
         x = self.fc2(x)
